c3669085bb60adb231df1b91b06e48d49cf0b8f7
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-25 16:56:38 -0400

    redeploy testing

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ PORT: process.env.PORT || 8000,
 NODE_ENV: process.env.NODE_ENV || 'development',
 DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/motive-api',
 TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,  
-JWT_SECRET: process.env.JWT_SECRET,
+JWT_SECRET: process.env.JWT_SECRET || 'motive-api-jwt-secret' ,
 JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 
 }
