fa23139d5c28a060c73a2ff1c92dbc53d590dbcc
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-25 14:22:43 -0400

    testing deployment

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ NODE_ENV: process.env.NODE_ENV || 'development',
 DATABASE_URL: process.env.DATABASE_URL ,
 TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,  
 JWT_SECRET: process.env.JWT_SECRET,
-JWT_EXPIRY: process.env.JWT_EXPIRY,
+JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 
 }
 
