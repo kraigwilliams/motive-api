766836105ca3a3ca4429020015489c6cac1e0823
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-26 18:20:14 -0400

    test sending response for post topic

--- src/topic/topic-router.js
+++ src/topic/topic-router.js
@@ -1,25 +1,21 @@
-const express = require('express')
-const path= require("path");
-const xss = require("xss")
-const TopicService = require('./topic-service')
-const { requireAuth } = require('../middleware/jwt-auth')
+const express = require("express");
+const path = require("path");
+const xss = require("xss");
+const TopicService = require("./topic-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
+const topicRouter = express.Router();
 
-const topicRouter = express.Router()
-
-const jsonBodyParser = express.json()
+const jsonBodyParser = express.json();
 
 const serializeTopic = topic => ({
-    id: Number(topic.id),
-    topic_title: xss(topic.topic_title),
-    topic_content: xss(topic.topic_content),
-    topic_owner:Number(topic.topic_owner)
-    
-  });
+  id: Number(topic.id),
+  topic_title: xss(topic.topic_title),
+  topic_content: xss(topic.topic_content),
+  topic_owner: Number(topic.topic_owner)
+});
 
-
-topicRouter
-  .use(requireAuth)
+topicRouter.use(requireAuth);
 
 //   .use(async (req, res, next) => {
 //     try {
@@ -41,87 +37,67 @@ topicRouter
 //   })
 
 topicRouter
-.route('/')
-  .get( async (req, res, next) => {
-      const knexInstance= req.app.get('db')
-      const userId=req.user.id
-      console.log("user id", req.user.id)
+  .route("/")
+  .get(async (req, res, next) => {
+    const knexInstance = req.app.get("db");
+    const userId = req.user.id;
+    console.log("user id", req.user.id);
     try {
-      const topics = await TopicService.getAllTopics(knexInstance,Number(req.user.id))
-     
-      
+      const topics = await TopicService.getAllTopics(
+        knexInstance,
+        Number(req.user.id)
+      );
 
-
-      res.json(topics.map(serializeTopic))
-        
-      
-    } 
-    catch (error) {
-      next(error)
+      res.json(topics.map(serializeTopic));
+    } catch (error) {
+      next(error);
     }
   })
 
-  .post(jsonBodyParser, async(req, res, next) => {
-      try{
-    const { topic_title, topic_content } = req.body;
-    const newTopic = { topic_title, topic_content };
+  .post(jsonBodyParser, async (req, res, next) => {
+    try {
+      const { topic_title, topic_content } = req.body;
+      const newTopic = { topic_title, topic_content };
 
-    for (const [key, value] of Object.entries(newTopic)) {
-      if (value == null) {
-        return res.status(400).json({
-          error: { message: `Missing '${key}' in request body` }
-        });
+      for (const [key, value] of Object.entries(newTopic)) {
+        if (value == null) {
+          return res.status(400).json({
+            error: { message: `Missing '${key}' in request body` }
+          });
+        }
       }
+      newTopic.topic_owner = req.user.id;
+
+      const topic = await TopicService.insertTopic(req.app.get("db"), newTopic);
+
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${topic.id}`))
+
+        .json(serializeTopic(topic));
+    } catch (error) {
+      next(error);
     }
-    newTopic.topic_owner = req.user.id;
-
-    const topic = await TopicService.insertTopic(req.app.get("db"), newTopic)
-    
-    
-    
-    
-        res
-          .status(201)
-          .location(path.posix.join(req.originalUrl, `/${topic.id}`))
-
-          .json(serializeTopic(topic));
-      
-
-      }
-      catch(error){
-        next(error)
-      }
-      
   });
 
-
-
-topicRouter
-.route('/:topicId')
-  .get( async (req, res, next) => {
-    
-    try {
-
- const knexInstance = req.app.get("db");
-       const topic=  await TopicService.getById(knexInstance, Number(req.params.topicId))
-       console.log("topic",topic, "then",req.params.topicId, "req.params")
-       if (!topic) {
-        return res.status(404).json({
-          error: { message: `This topic does not exist.` }
-        });
-      }
-      
-      res.json(serializeTopic(topic));
-      
-    
-    } 
-    catch(error) {
-      next(error)
+topicRouter.route("/:topicId").get(async (req, res, next) => {
+  try {
+    const knexInstance = req.app.get("db");
+    const topic = await TopicService.getById(
+      knexInstance,
+      Number(req.params.topicId)
+    );
+    console.log("topic", topic, "then", req.params.topicId, "req.params");
+    if (!topic) {
+      return res.status(404).json({
+        error: { message: `This topic does not exist.` }
+      });
     }
-    
 
-    
-  })
+    res.json(serializeTopic(topic));
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-module.exports = topicRouter
+module.exports = topicRouter;
--- src/topic/topic-service.js
+++ src/topic/topic-service.js
@@ -1,33 +1,41 @@
-const TopicService= {
-    getAllTopics(knex,id){
-        return knex.from('topic').select('*')
-         .where('topic_owner',id)
-    },
+const TopicService = {
+  getAllTopics(knex, id) {
+    return knex
+      .from("topic")
+      .select("*")
+      .where("topic_owner", id);
+  },
 
-    insertTopic(knex,newTopic){
+  insertTopic(knex, newTopic) {
+    return knex
+      .insert(newTopic)
+      .into("topic")
+      .returning("*")
+      .then(rows=>{
+          return rows[0]
+      })
+      ;
+  },
 
-return knex
-.insert(newTopic)
-.into("topic")
-.returning("*")
-    },
+  getById(knex, topicId) {
+    return knex
+      .from("topic")
+      .select("topic.*")
+      .first()
+      .where("id", topicId)
+      .returning("*");
+  },
 
-    getById(knex,topicId){
-        return knex.from('topic').select('topic.*').first().where('id', topicId).returning("*")
-    },
-    
-    deleteTopic(knex,topicId){
-        return knex('topic')
-        .where('id',topicId)
-        .delete()
-    },
-    updateTopic(knex,id, newTopicFields){
-        return knex('topics')
-        .where({id})
-        .update(newTopicFields)
-    }
+  deleteTopic(knex, topicId) {
+    return knex("topic")
+      .where("id", topicId)
+      .delete();
+  },
+  updateTopic(knex, id, newTopicFields) {
+    return knex("topics")
+      .where({ id })
+      .update(newTopicFields);
+  }
+};
 
-
-}
-
-module.exports= TopicService
+module.exports = TopicService;
