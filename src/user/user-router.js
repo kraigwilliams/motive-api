e5cc644c4db6ee229512ee4156461be8bc000e90
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-01 17:22:26 -0400

    test user registration

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -49,6 +49,7 @@ userRouter
         .location(path.posix.join(req.originalUrl, `/${user.id}`))
         .json(UserService.serializeUser(user))
     } catch(error) {
+      console.log("user error",error)
       next(error)
     }
   })
