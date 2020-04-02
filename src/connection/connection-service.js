691d850b4396b6960abdf71989819bbc91b94a11
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-02 13:31:30 -0400

    test connections query

--- src/connection/connection-service.js
+++ src/connection/connection-service.js
@@ -29,8 +29,8 @@ const ConnectionService= {
               //.from('fokul_users')
               //.whereNot({'fokul_users.id' : userId})
               //.whereNotExists(function(){
-                .select('*').from('connections').where({'connections.sender_id' : '2', 'connections.receiver_id' : userId})
-                  .orWhere({'connections.sender_id' : userId, 'connections.receiver_id' : '2'});
+                .select('*').from('connections').where({'connections.sender_id' : '1', 'connections.receiver_id' : userId})
+                  .orWhere({'connections.sender_id' : userId, 'connections.receiver_id' : '1'});
               //});       
           
   
