15f3faa29fae1d01b6b321cb2143ea9240791bb9
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-02 13:29:15 -0400

    test connections query

--- src/connection/connection-service.js
+++ src/connection/connection-service.js
@@ -25,13 +25,13 @@ const ConnectionService= {
         getNonConnections(knex,userId){
             
             knex
-              .select('*')
-              .from('fokul_users')
-              .whereNot({'fokul_users.id' : userId})
-              .whereNotExists(function(){
-                this.select('*').from('connections').where({'connections.sender_id' : 'fokul_users.id', 'connections.receiver_id' : userId})
+              //.select('*')
+              //.from('fokul_users')
+              //.whereNot({'fokul_users.id' : userId})
+              //.whereNotExists(function(){
+                select('*').from('connections').where({'connections.sender_id' : 'fokul_users.id', 'connections.receiver_id' : userId})
                   .orWhere({'connections.sender_id' : userId, 'connections.receiver_id' : 'fokul_users.id'});
-              });       
+              //});       
           
   
         
