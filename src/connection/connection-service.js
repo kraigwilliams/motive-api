39700ee20d58124a96bcfb282aad10034c2f7366
Jordan Castillo <jordanxcallaway@gmail.com>
2020-04-02 14:17:05 -0700

    testing nonconnections

--- src/connection/connection-service.js
+++ src/connection/connection-service.js
@@ -32,7 +32,11 @@ const ConnectionService= {
         where fokul_users.id != ${userId}
         and not exists (select 1 from connections where (connections.sender_id = fokul_users.id and connections.receiver_id = ${userId})
         or (connections.sender_id = ${userId} and connections.receiver_id = fokul_users.id))`
-      ); 
+      )
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      }); 
   },
 //   getNonConnections (knex, userId) {
 //     return this.getAllConnections(knex, userId)
