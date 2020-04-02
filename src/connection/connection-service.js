8f88d903560fc05360d267b8158412d3d67bf5e2
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-01 20:33:53 -0400

    test non connections

--- src/connection/connection-service.js
+++ src/connection/connection-service.js
@@ -31,9 +31,9 @@ const ConnectionService= {
         .leftOuterJoin('connections','fokul_users.id', "connections.receiver_id")
         
         whereNot({
-        //     {'connections.sender_id':senderId,
-           'fokul_users.id':senderId
-        //       'connections.receiver_id':senderId 
+        'connections.sender_id':senderId,
+           'fokul_users.id':senderId,
+           'connections.receiver_id':senderId 
          })
 
             //   'fokul_users.id':'connections.receiver_id',
