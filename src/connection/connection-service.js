cd56321a008fd80d01eb81012785b576faa0bffa
Jordan Castillo <jordanxcallaway@gmail.com>
2020-04-02 12:02:18 -0700

    testing nonconnections

--- src/connection/connection-service.js
+++ src/connection/connection-service.js
@@ -31,7 +31,7 @@ const ConnectionService= {
       .whereNot({'fokul_users.id' : userId})
       .whereNotExists(function(){
         this.select('*').from('connections')
-          .whereRaw('connections.sender_id = fokul_users.id');
+          .whereRaw(/*'connections.sender_id = fokul_users.id', */`connections.receiver_id = ${userId}`);
         //   .orWhere({'connections.receiver_id' : userId});
         //.orWhere({'connections.sender_id' : userId, 'connections.receiver_id' : 'fokul_users.id'});
       }); 
