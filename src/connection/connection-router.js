cb6ba754c5525f0a992f14d40b2b2bf12f9002b3
Jordan Castillo <jordanxcallaway@gmail.com>
2020-04-03 12:32:33 -0700

    setting up add new connection

--- src/connection/connection-router.js
+++ src/connection/connection-router.js
@@ -46,12 +46,23 @@ connectionRouter
       )
 
       console.log(connections, 'connections to filter through')
-      const alreadyAdded = !!connections.filter(connect => {
-       (connect.receiver_id == connectionId 
-        && connect.sender_id == userId) ||  
-       (connect.sender_id == connectionId 
-        && connect.receiver_id == userId)
-      })
+      // const alreadyAdded = !!connections.filter(connect => {
+      //  (connect.receiver_id == connectionId 
+      //   && connect.sender_id == userId) ||  
+      //  (connect.sender_id == connectionId 
+      //   && connect.receiver_id == userId)
+      // })
+      const alreadyAdded = connections.filter(function(connect) {
+        if (connect.receiver_id == connectionId 
+          && connect.sender_id == userId) {
+            return true;
+          } else if (connect.sender_id == connectionId 
+          && connect.receiver_id == userId) {
+            return true
+          }  else {
+            return false
+          }
+       })
       console.log(alreadyAdded, 'already added connections')
 
       if(!alreadyAdded) {
