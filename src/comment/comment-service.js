c0b617c81d0d1f43aac6a353f0dedfadbb92f6a0
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-06 13:54:16 -0400

    test comment backend

--- src/comment/comment-service.js
+++ src/comment/comment-service.js
@@ -1,12 +1,22 @@
 const CommentService={
 
 getComments(knex,thoughtId){
-return knex.from('comments').select('*')
-.join("fokul_users")
+return knex.raw(
+    `
+    SELECT fokul_users.username, fokul_users.first_name, fokul_users.last_name, comments.comment_content, comments.date_posted FROM comments JOIN fokul_users ON comments.commenter_id = fokul_users.id WHERE comments.thought_id = ${thoughtId};
+    `
+)
+.then(results => {
+    return results.rows;
+  });
 
-.where({"comments.commenter_id":"fokul_users.id",
-"thought_id": thoughtId
-})
+
+    // return knex.from('comments')
+// .join("fokul_users","fokul_users.id","=","comments_commenter_id")
+// .select('')
+// .where({"comments.commenter_id":"fokul_users.id",
+// "thought_id": thoughtId
+// })
 },
 
 insertComment(knex,newComment){
