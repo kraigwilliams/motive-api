44fb299e7de74e48f966deed80f378483c64b97b
Jordan Castillo <jordanxcallaway@gmail.com>
2020-04-07 15:24:27 -0700

    testing shared users for thought

--- src/thought/thought-service.js
+++ src/thought/thought-service.js
@@ -1,7 +1,7 @@
 /* eslint-disable strict */
 const ThoughtService = {
   getAllThoughts(knex, id) {
-    return knex.from('thought').select('*').where('thought_owner', id);
+    return knex.from("thought").select("*").where("thought_owner", id);
   },
 
   getSharedThoughts(knex, id) {
@@ -20,8 +20,8 @@ const ThoughtService = {
   insertThought(knex, newThought) {
     return knex
       .insert(newThought)
-      .into('thought')
-      .returning('*')
+      .into("thought")
+      .returning("*")
       .then((rows) => {
         return rows[0];
       });
@@ -29,15 +29,28 @@ const ThoughtService = {
 
   getById(knex, thoughtId) {
     return knex
-      .select(
-        'thought.id, thought.thought_title, thought.thought_owner, thought.thought_topic, thought.level, thought.date_modified, fokul_users.username, fokul_users.first_name, fokul_users.last_name'
+      .raw(
+        `SELECT *
+FROM thought
+LEFT JOIN fokul_users ON thought.thought_owner = fokul_users.id
+WHERE thought.id = ${thoughtId};`
       )
-      .from('thought')
-      .join('fokul_users', 'thought.thought_owner', '=', 'fokul_users.id')
-      .where('thought.id', thoughtId)
-      .first()
-      .returning('*');
+      .then((results) => {
+        return results.rows;
+      });
   },
+
+  // getById(knex, thoughtId) {
+  //   return knex
+  //     .select(
+  //       'thought.id, thought.thought_title, thought.thought_owner, thought.thought_topic, thought.level, thought.date_modified, fokul_users.username, fokul_users.first_name, fokul_users.last_name'
+  //     )
+  //     .from('thought')
+  //     .join('fokul_users', 'thought.thought_owner', '=', 'fokul_users.id')
+  //     .where('thought.id', thoughtId)
+  //     .first()
+  //     .returning('*');
+  // },
 
   getSharedUsersById(knex, thoughtId) {
     return knex
@@ -55,32 +68,32 @@ const ThoughtService = {
 
   getThoughtLevel(knex, thoughtId, userId) {
     return knex
-      .from('thought_connections')
-      .select('thought_connections.level')
+      .from("thought_connections")
+      .select("thought_connections.level")
       .where({ shared_userId: userId, thought_id: thoughtId })
-      .returning('*')
+      .returning("*")
       .first();
   },
 
   updateThought(knex, thoughtId, newNoteFields) {
-    return knex('thought')
-      .where('id', thoughtId)
+    return knex("thought")
+      .where("id", thoughtId)
       .update(newNoteFields)
-      .returning('*')
+      .returning("*")
       .then((rows) => {
         return rows[0];
       });
   },
 
   deleteThought(knex, thoughtId) {
-    return knex('thought').where('id', thoughtId).delete();
+    return knex("thought").where("id", thoughtId).delete();
   },
 
   shareThought(knex, sharedThought) {
     return knex
       .insert(sharedThought)
-      .into('thought_connections')
-      .returning('*')
+      .into("thought_connections")
+      .returning("*")
       .then((rows) => {
         return rows[0];
       });
